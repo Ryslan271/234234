5c235301cb135faa6cc766cc1fbfa522a2b7f72e
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-08 14:24:14 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+import os
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('Osnova.html')
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def lyshee():
+    return render_template('Osnova.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
