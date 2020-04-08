72cae9433e3bb7c3f7fd5be8143c2c8175801fec
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-08 19:52:02 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -15,6 +15,16 @@ def lyshee():
     return render_template('Lyshee.html')
 
 
+@app.route("/O nas")
+def onas():
+    return render_template('O nas.html')
+
+
+@app.route("/Contact")
+def contact():
+    return render_template('Contact.html')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
